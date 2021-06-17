9fa8dd93a5223ff9dfa4245d860b96063d45891b
Yasin Arafat <yasinarafat1194@gmail.com>
2021-06-17 14:13:12 +0600

    app.js error handling

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const { message } = require('./schema/signup');
 const app=express();
 app.use(express.json());
 const appsingnupschema=require('./schema/signup');
@@ -9,12 +10,23 @@ app.post('/api/v1/user',async(req,res,next)=>{
     }catch(err){
         console.log('error found in input data format request canceled');
         console.log(err);
-        return err;
+        return next(err);
     }
     return next();
 },(req,res,next)=>{
     console.log('request accepted');
     console.log(req.body);
+    res.end();
+});
+app.use((err,req,res,next)=>{
+    res.status(400).send({
+        error:{
+            status:400,
+            name:err.name,
+            message:err.message
+
+        },
+    });
     res.end();
 });
 module.exports=app;
