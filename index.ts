47a47a0554c0ec3bbf8b15378c4d87abc513becb
Yasin Arafat <yasinarafat1194@gmail.com>
2021-06-17 23:32:43 +0600

    entity

--- index.ts
+++ index.ts
@@ -1,7 +1,24 @@
 const http=require('http');
 import 'reflect-metadata';
+import {createConnection} from 'typeorm';
 import app from './src/app';
+import User from './src/entityes/User';
 const server=http.createServer(app);
+(
+    async function(){
+        await createConnection({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: '12',
+            database: 'nodejsdb',
+            entities: [User],
+            synchronize: true,
+            logging: false
+        });
+    }
+)();
 
 server.listen(3000, ()=>{
     console.log('server is listening at port 3000');
---
+++ src/entityes/User.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export default class User{
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    name:string;
+
+    @Column()
+    email:string;
+    
+};
